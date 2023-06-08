741c38ab0956ea6f93868877a81bb860027ae2ed
BelierJavier <Zathoroth@icloud.com>
2023-06-08 16:09:52 -0400

    Load Datasets
    +Added libraries
    +Load training and testing datasets

--- vigil.py
+++ vigil.py
@@ -1,9 +1,23 @@
-#Import Libraries
+# Import Libraries
+
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd
+import seaborn as sb
+import sklearn
+import imblearn
 
-#Loading Datasets
-train = pd.read_csv('/Vigil/Vigil/datasets/Train_data.csv')
-test = pd.read_csv('/Vigil/Vigil/datasets/Test_data.csv')
+# Settings
+pd.set_option('display.max_columns', None)
+np.set_printoptions(precision=3)
+sb.set(style='darkgrid')
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+# Loading Datasets
+train = pd.read_csv('datasets/Train_data.csv')
+test = pd.read_csv('datasets/Test_data.csv')
 
 print(train.head(4))
