54bf61b5969179c233b6fac191a5a16ac91dfba8
BelierJavier <Zathoroth@icloud.com>
2023-06-09 13:20:18 -0400

    Upgraded Datasets
    +Added CIC-IDS-2017 dataset
    -Removed NSL-KDD-1999 dataset

--- vigil.py
+++ vigil.py
@@ -18,16 +18,8 @@ plt.rcParams['xtick.labelsize'] = 12
 plt.rcParams['ytick.labelsize'] = 12
 
 # Loading Datasets
-train = pd.read_csv('datasets/Train_data.csv')
-test = pd.read_csv('datasets/Test_data.csv')
-train.drop(['num_outbound_cmds'], axis=1, inplace=True) # Redundant Data
-test.drop(['num_outbound_cmds'], axis=1, inplace=True) # Redundant Data
-train.drop_duplicates(subset=train.columns[1:],inplace=True) # Redundant Data
-print(kl.missingval_plot(train,figsize=(6,5))) # Double-Check Missing Values
+train = pd.read_csv('')
+test = pd.read_csv('')
 
-
-# Display Data
-print(train['class'].value_counts())
-print(train.shape)
 
 
