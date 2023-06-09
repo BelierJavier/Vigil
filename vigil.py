7ee10f3a22348386de48cc5174222344cfdf4f17
BelierJavier <Zathoroth@icloud.com>
2023-06-09 13:30:29 -0400

    Loading New Data
    +Loaded CIC-IDS-2017 datasets

--- vigil.py
+++ vigil.py
@@ -18,8 +18,14 @@ plt.rcParams['xtick.labelsize'] = 12
 plt.rcParams['ytick.labelsize'] = 12
 
 # Loading Datasets
-train = pd.read_csv('')
-test = pd.read_csv('')
+data1 = pd.read_csv('datasets/Monday-WorkingHours.pcap_ISCX.csv')
+data2 = pd.read_csv('datasets/Tuesday-WorkingHours.pcap_ISCX.csv')
+data3 = pd.read_csv('datasets/Wednesday-WorkingHours.pcap_ISCX.csv')
+data4 = pd.read_csv('datasets/Thursday-WorkingHours-Morning-WebAttacks.pcap_ISCX.csv')
+data5 = pd.read_csv('datasets/Thursday-WorkingHours-Afternoon-Infilteration.pcap_ISCX.csv')
+data6 = pd.read_csv('datasets/Friday-WorkingHours-Morning.pcap_ISCX.csv')
+data7 = pd.read_csv('datasets/Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv')
+data8 = pd.read_csv('datasets/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv')
 
 
 
