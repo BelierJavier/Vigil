b3313d5001559b00d0354ea9604038ed9d706381
BelierJavier <Zathoroth@icloud.com>
2023-11-04 10:54:40 -0400

    Updated Mac Packet Sniffer
    +Packet sniffing session initializes
    -Packet Sniffing session does not capture packets

--- vigil-mac.py
+++ vigil-mac.py
@@ -8,10 +8,21 @@ import struct
 
 #knn_loaded = pickle.load('ids_model','rb')
 
-def sniff():
+def sniff(interface):
     sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
     sniffer.bind((interface, 0))
     sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    
+    try:
+        while True:
+            print("Currently Scanning")
+            raw_packet = sniffer.recvfrom(65565)
+            dest_ip, src_ip, proto, data = unpack_frame(raw_packet[0])
+            print(f"Source IP: {src_ip}, Destination IP: {dest_ip}, Protocol: {proto}")
+            print(f"Data: {data}")
+
+    except KeyboardInterrupt:
+        print("Packet Capture Stopped")
 
 def unpack_frame(raw_packet):
     header_length = (raw_packet[0] & 15) * 4
@@ -20,5 +31,5 @@ def unpack_frame(raw_packet):
     return socket.inet_ntoa(dest_ip), socket.inet_ntoa(src_ip), proto, data
 
 if __name__ == "__main__" :
-    interface = "lo0"
+    interface = "en0"
     sniff(interface)
