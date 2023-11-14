c5be803443b820e597666cd704251a24b0b147d0
BelierJavier <Zathoroth@icloud.com>
2023-11-14 16:33:12 -0500

    Packet Sniffing Phase Complete
    + completed packet sniffer program

--- vigil-linux.py
+++ vigil-linux.py
@@ -4,9 +4,21 @@
 #import pickle
 import socket
 import struct
-#import textwrap
+import textwrap
 
 #knn_loaded = pickle.load('ids_model','rb')
+
+TAB_1 = '\t - '
+TAB_2 = '\t\t - '
+TAB_3 = '\t\t\t - '
+TAB_4 = '\t\t\t\t - '
+
+DATA_TAB_1 = '\t  '
+DATA_TAB_2 = '\t\t  '
+DATA_TAB_3 = '\t\t\t  '
+DATA_TAB_4 = '\t\t\t\t  '
+
+
 
 class Stream():
     pass
@@ -23,6 +35,19 @@ def sniff():
             print('\nEthernet Frame:')
             print('{} : TCP | Destination: {}, Source: {}, Protocol: {}'.format(i, dest_mac, src_mac, eth_proto))
             i += 1
+        
+            try:
+                (src_port, dest_port, sequence, acknowledgment, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data) = tcp_segment(data)
+                print(TAB_1 + 'TCP Segment: ')
+                print(TAB_2 + 'Source Port: {}, Destination port: {}'.format(src_port, dest_port))
+                print(TAB_2 + 'Sequence: {}, Acknowledgment: {}'.format(sequence, acknowledgment))
+                print(TAB_2 + 'Flags: ')
+                print(TAB_3 + 'URG: {}, ACK: {}, PSH: {}, RST: {}, SYN: {}, FIN: {}'.format(flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin))
+                print(TAB_2 + 'Data: ')
+                print(format_multi_line(DATA_TAB_3, data))
+            
+            except struct.error:
+                print('Struct Error Occurred: Not a TCP Packet')
     
     except KeyboardInterrupt:
         print('Packet Capture Stopped')
@@ -36,6 +61,47 @@ def unpack_frame(data):
 def get_mac_addr(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr) # Takes all chunks & formats them properly to 2 decimal places
     return ':'.join(bytes_str).upper() # Joins formatted chunks together to form MAC addr with ':'
+
+# UNPACK IPV4 PACKET
+def ipv4_packet(data):
+    version_header_length = data[0] # Reads version header length
+    version = version_header_length >> 4 # Reads Version by shifting 4 bytes to right from version header length
+    header_length = (version_header_length & 15) * 4 # Reads entire header length
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20]) # Unpacks header data
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:] # Returning header data
+
+# RETURNING IP ADDRESS INTO READABLE FORMAT (ex. 127.0.0.1)
+def ipv4(addr):
+    return '.'.join(map(str, addr))
+
+# UNPACKIN TCP PACKETS
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgment, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 5
+    flag_psh = (offset_reserved_flags & 8) >> 5
+    flag_rst = (offset_reserved_flags & 4) >> 5
+    flag_syn = (offset_reserved_flags & 2) >> 5
+    flag_fin = offset_reserved_flags & 1
+
+    return src_port, dest_port, sequence, acknowledgment, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
+
+# UNPACKING ICMP PACKETS
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+# FORMATTING LONG LINES OF DATA FOR BETTER VISUALIZATION
+def format_multi_line(prefix, string, size=80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        if size % 2:
+            size -= 1
+    return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
+
+
 
 if __name__ == "__main__":
     sniff()
